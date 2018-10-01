73530c41cb50bb9076b5936f743ef108477fc773
casey bradford <casey.bradford@wework.com>
2018-10-01 22:17:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ main.remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 
-// document.body.appendChild(element)
+document.body.appendChild(element)
 
-// var h1 = document.createElement('h1')
+var h1 = document.createElement('h1')
 
-// element.appendChild(h1)
+element.appendChild(h1)
 
-// h1.innerHTML = 'HEY'
+h1.innerHTML = 'HEY'
 
-var element = document.createElement("h1");
-element.appendChild(document.createTextNode('The man who mistook his wife for a hat'));
-document.getElementById('main').appendChild(element);
+
 
 
 
