70f0130f8ecf8473e031731ac63a2c6681d0e7dd
casey bradford <casey.bradford@wework.com>
2018-10-01 22:04:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+document.body.appendChild(element)
+
+var h1 = document.createElement('h1')
+
+element.appendChild(h1)
+
