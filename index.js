e2df73c94a2376c13d0bd3c0d501b0668d77e516
casey bradford <casey.bradford@wework.com>
2018-10-01 22:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 main.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = document.createElement('div');
+const newHeader = document.createElement('h1');
 
 document.body.appendChild(newHeader)
 
